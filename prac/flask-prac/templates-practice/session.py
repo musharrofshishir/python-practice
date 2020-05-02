4c3cad13790e5e741283a0fe5e6395c83147bf2a
Musharrof Shishir <musharrofshishir@users.noreply.github.com>
2020-05-03 01:04:11 +0600

    Update session.py

--- prac/flask-prac/templates-practice/session.py
+++ prac/flask-prac/templates-practice/session.py
@@ -1,22 +1,22 @@
 from flask import Flask, render_template, request, session
-# from flask_session import Session 
+from flask_session import Session 
 app = Flask(__name__)
+
+app.secret_key = 'bugisugi' #must add a secret key
 
 # app.config["SESSION_PERMANENT"] = "false"
 # app.config["SESSION_TYPE"] = "filesystem"
 # Session(app)
 
-notes = []
-
 @app.route("/", methods=[ "GET", "POST" ])
 def todos():
-    # if session.get("notes") is None:
-    #     session["notes"] = []
+    if session.get("notes") is None:
+        session["notes"] = []
     if request.method == "POST":
         note = request.form.get("note")
-        # session["notes"].append(note)
-        notes.append(note)
-    return render_template("todo.html", notes=notes)
+        session["notes"].append(note)
+
+    return render_template("todo.html", notes=session["notes"])
 
 
 if __name__ == "__main__":
